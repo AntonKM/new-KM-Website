d29d2330ab2e2f04477c0674d6ed4f4f4836952a
AntonKM <ponomarev-anton@outlook.de>
2025-02-25 18:07:18 +0100

    refactor global styles and update site description with additional keywords

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,4 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+
 
 export const SITE_TITLE = 'Keks Magazin';
-export const SITE_DESCRIPTION = 'Willkommen zur Keks Magazin Website!';
+export const SITE_DESCRIPTION = 'Willkommen zur Keks Magazin Website! Wir machen den ULMCAST Podcast und schreiben Artikel über Ulm und Neu-Ulm.';
