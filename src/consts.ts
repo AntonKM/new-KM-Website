a07f8d0df991521ca03a3c7c89f032085c5cd9e2
AntonKM <ponomarev-anton@outlook.de>
2025-02-27 14:34:52 +0100

    add search functionality and update site constants

--- src/consts.ts
+++ src/consts.ts
@@ -2,3 +2,5 @@
 
 export const SITE_TITLE = 'Keks Magazin';
 export const SITE_DESCRIPTION = 'Willkommen zur Keks Magazin Website! Wir machen den ULMCAST Podcast und schreiben Artikel über Ulm und Neu-Ulm.';
+export const SITE_URL ='https://www.keks-magazin.de';
+export const SITE_KEYWORDS = 'Ulm, Neu-Ulm, Podcast, Keks, Magazin, Ulmcast, Artikel, Blog, Keks Magazin, Keks-Magazin, KeksMagazin, Keks-Magazin.de, KeksMagazin.de';
