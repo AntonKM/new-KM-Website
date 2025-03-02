890c6c7159f81bed73226c3cbcfc507eb061bb29
AntonKM <ponomarev-anton@outlook.de>
2025-03-02 11:12:12 +0100

    refactor blog routing and navigation structure; replace HeaderLink with Hamburger component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-// @ts-check
+
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import pagefind from "astro-pagefind"
 
-// https://astro.build/config
 export default defineConfig({
 	site: 'http://www.keks-magazin.de',
 	integrations: [mdx(), sitemap(), pagefind()],
