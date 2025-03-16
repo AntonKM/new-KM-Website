fbb3e84da790d326ad8e3c14dcd81de2f993997d
AntonKM <ponomarev-anton@outlook.de>
2025-03-16 13:36:34 +0100

    refactor: remove unused constants from consts.ts

--- src/consts..d.ts
+++
@@ -1,6 +0,0 @@
-
-
-export const SITE_TITLE = 'Keks Magazin';
-export const SITE_DESCRIPTION = 'Willkommen zur Keks Magazin Website! Wir machen den ULMCAST Podcast und schreiben Artikel über Ulm und Neu-Ulm.';
-export const SITE_URL ='https://www.keks-magazin.de';
-export const SITE_KEYWORDS = 'Ulm, Neu-Ulm, Podcast, Keks, Magazin, Ulmcast, Artikel, Blog, Keks Magazin, Keks-Magazin, KeksMagazin, Keks-Magazin.de, KeksMagazin.de, keks-magazin .de, keksMagazin, Keks, Magazin, Nachrichten, Artikel, Blog, Stadt Ulm, Stadt Neu-Ulm, tolle Podcasts über Politik, Podcasts über Politik, Podcasts aus Ulm, UlmCast, Ulm Cast, Podcasts aus Neu-Ulm';
