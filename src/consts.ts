9d9e2234d9caa25abbf5bccd5bccd0b1ce383cc8
AntonKM <ponomarev-anton@outlook.de>
2025-04-23 11:56:08 +0200

    Refactor code structure for improved readability and maintainability

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,11 @@
 
-
 export const SITE_TITLE = 'Keks Magazin';
-export const SITE_DESCRIPTION = 'Willkommen zur Keks Magazin Website! Wir machen den ULMCAST Podcast und schreiben Artikel über Ulm und Neu-Ulm.';
+export const SITE_DESCRIPTION = 'Willkommen zur Keks Magazin Website! Wir machen den ULMCAST Podcast und schreiben Artikel über Ulm und Neu-Ulm. Hier findest du spannende Themen und interessante Einblicke in unsere Stadt.';
 export const SITE_URL ='https://www.keks-magazin.de';
 export const SITE_KEYWORDS = 'Ulm, Neu-Ulm, Podcast, Keks, Magazin, Ulmcast, Artikel, Blog, Keks Magazin, Keks-Magazin, KeksMagazin, Keks-Magazin.de, KeksMagazin.de, keks-magazin .de, keksMagazin, Keks, Magazin, Nachrichten, Artikel, Blog, Stadt Ulm, Stadt Neu-Ulm, tolle Podcasts über Politik, Podcasts über Politik, Podcasts aus Ulm, UlmCast, Ulm Cast, Podcasts aus Neu-Ulm';
+export const MENU_ITEMS = [
+    { label: 'Startseite', url: '/'},
+    { label: 'Podcast', url: '/podcast'},
+    { label: 'Über uns', url: '/about'},
+    { label: 'Impressum', url: '/impressum'},
+];
